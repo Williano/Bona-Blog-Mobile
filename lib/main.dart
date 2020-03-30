73e56423c03dea2674a17b01cdede070c9ecd79c
Williano <paawilly17@gmail.com>
2020-03-29 19:44:58 -0500

    Update Name

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(BonaBlog());
 
-class MyApp extends StatelessWidget {
+class BonaBlog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
