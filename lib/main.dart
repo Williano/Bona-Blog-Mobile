2aeb6a91b557eb2b012d674e2f1cbab89acb6248
Williano <paawilly17@gmail.com>
2020-03-29 20:54:53 -0500

    Add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bona_blog/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BonaBlog());
@@ -10,7 +11,7 @@ class BonaBlog extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
