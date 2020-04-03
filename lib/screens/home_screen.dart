c2ead45ee975eb5bbcf80fc2da19b5aa554a1c5b
Williano <paawilly17@gmail.com>
2020-04-03 17:46:07 -0500

    Add bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,54 @@
+import 'package:bona_blog/screens/feed_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentTabIndex = 2;
+
+  final PageStorageBucket bucket = PageStorageBucket();
+
+  List<Widget> screens = [
+    FeedScreen(key: PageStorageKey("Feed Screen")),
+    FeedScreen(key: PageStorageKey("Feed Screen")),
+    FeedScreen(key: PageStorageKey("Feed Screen")),
+    FeedScreen(key: PageStorageKey("Feed Screen")),
+    FeedScreen(key: PageStorageKey("Feed Screen")),
+  ];
+
+  List<BottomNavigationBarItem> _bottomNavigationBarItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text("Categories")),
+    BottomNavigationBarItem(icon: Icon(Icons.people), title: Text("Authors")),
+    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Feed")),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.dashboard), title: Text("Dashboard")),
+    BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("Settings"))
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("Home"),
+      body: PageStorage(
+        bucket: bucket,
+        child: screens[_currentTabIndex],
       ),
-      body: Container(),
+      bottomNavigationBar: _appBottomNavigationBar(),
     );
   }
+
+  Widget _appBottomNavigationBar() => BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentTabIndex,
+        items: _bottomNavigationBarItems,
+        onTap: (int index) {
+          setState(() {
+            _currentTabIndex = index;
+          });
+        },
+      );
 }
