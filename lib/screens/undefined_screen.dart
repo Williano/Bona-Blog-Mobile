914cafb575cbb9dca5ccdea484a15de5c4c4363c
Williano <paawilly17@gmail.com>
2020-04-04 02:58:19 -0500

    Update

--- lib/screens/undefined_screen.dart
+++ lib/screens/undefined_screen.dart
@@ -16,33 +16,50 @@ class UndefinedScreen extends StatelessWidget {
         title: Text("$name Not Found"),
         centerTitle: true,
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-                child: Text(
-              "Route for $name is not defined",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            )),
-            Center(
-              child: RaisedButton.icon(
-                  elevation: 0.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, HomeScreenRoute);
-                  },
-                  color: customColor,
-                  icon: Icon(
-                    Icons.home,
-                    color: Colors.white,
-                  ),
-                  label: Text(
-                    "Return To Feed",
-                    style: TextStyle(color: Colors.white, fontSize: 18.0),
-                  )),
-            )
-          ]),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/error3.jpg"),
+                    fit: BoxFit.fill)),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+          Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Route for $name is not defined",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColorLight),
+                  textAlign: TextAlign.center,
+                ),
+                Center(
+                  child: RaisedButton.icon(
+                      elevation: 0.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, HomeScreenRoute);
+                      },
+                      color: customColor,
+                      icon: Icon(
+                        Icons.home,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        "Return To Feed",
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      )),
+                )
+              ]),
+        ],
+      ),
     );
   }
 }
