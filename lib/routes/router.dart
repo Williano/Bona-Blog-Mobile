f71ac00e5994b7bc7252085b0a40a4c641fdedf3
Williano <paawilly17@gmail.com>
2020-04-05 17:39:57 -0500

    Add new route

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,3 +1,5 @@
+import 'package:bona_blog/screens/category_articles_list_screen.dart';
+import 'package:bona_blog/screens/category_list_screen.dart';
 import 'package:bona_blog/screens/feed_screen.dart';
 import 'package:bona_blog/screens/home_screen.dart';
 import 'package:bona_blog/screens/undefined_screen.dart';
@@ -12,6 +14,27 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
 
     case FeedScreenRoute:
       return MaterialPageRoute(builder: (BuildContext context) => FeedScreen());
+      break;
+
+    case CategoriesScreenRoute:
+      return MaterialPageRoute(
+          builder: (BuildContext context) => CategoryListScreen());
+      break;
+
+    case CategoryArticlesListRoute:
+      final Map<String, dynamic> data =
+          routeSettings.arguments as Map<String, dynamic>;
+
+      final Map<String, dynamic> categoryData = {
+        "categoryName": data["categoryName"],
+        "categoryImageURL": data["categoryImageURL"],
+      };
+
+      return MaterialPageRoute(
+          builder: (BuildContext contetxt) => CateoryArticlesListScreen(
+                categoryName: categoryData["categoryName"],
+                categoryImageURL: categoryData["categoryImageURL"],
+              ));
       break;
 
     default:
