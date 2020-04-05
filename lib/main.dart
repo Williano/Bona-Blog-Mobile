c4c833c0d82b6b45365bac9f0717f7cc7bd3caa0
Williano <paawilly17@gmail.com>
2020-04-05 04:55:24 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bona_blog/routes/router.dart';
 import 'package:bona_blog/screens/home_screen.dart';
 import 'package:bona_blog/utilities/custom_material_color.dart';
 import 'package:bona_blog/utilities/route_constants.dart';
+// import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,10 +13,18 @@ void main() {
       .then((_) => runApp(BonaBlog()));
 }
 
+// void main() => runApp(
+//       DevicePreview(
+//         builder: (context) => BonaBlog(),
+//       ),
+//     );
+
 class BonaBlog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // locale: DevicePreview.of(context).locale, // <--- Add the locale
+      // builder: DevicePreview.appBuilder,
       onGenerateRoute: generateRoute,
       initialRoute: HomeScreenRoute,
       debugShowCheckedModeBanner: false,
