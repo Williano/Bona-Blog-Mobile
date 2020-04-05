8323d17a38f321df5ae28b22bf90cefaad2fa253
Williano <paawilly17@gmail.com>
2020-04-05 17:43:57 -0500

    Add implementation for category article list

---
+++ lib/screens/category_articles_list_screen.dart
@@ -0,0 +1,48 @@
+import 'package:bona_blog/models/article_model.dart';
+import 'package:bona_blog/widgets/custom_sliver_app_bar_widget.dart';
+import 'package:bona_blog/widgets/custom_sliver_list_widget.dart';
+import 'package:flutter/material.dart';
+
+class CateoryArticlesListScreen extends StatefulWidget {
+  final String categoryName;
+  final String categoryImageURL;
+
+  const CateoryArticlesListScreen(
+      {Key key, @required this.categoryName, @required this.categoryImageURL})
+      : super(key: key);
+
+  @override
+  _CateoryArticlesListScreenState createState() =>
+      _CateoryArticlesListScreenState();
+}
+
+class _CateoryArticlesListScreenState extends State<CateoryArticlesListScreen> {
+  Map<String, dynamic> _categoryData;
+  List<Article> _articles;
+
+  @override
+  void initState() {
+    _categoryData = {
+      "categoryName": widget.categoryName,
+      "categoryImageURL": widget.categoryImageURL
+    };
+    _articles = Article.getAllArticles();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          customSliverAppBar(
+              context: context,
+              title: _categoryData["categoryName"],
+              assetImage: _categoryData["categoryImageURL"]),
+          customSliverList(context: context, articles: _articles),
+        ],
+      ),
+    );
+  }
+}
