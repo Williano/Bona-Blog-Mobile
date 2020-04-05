d8ed59aa71763b7265e334ee339b3ac7d43c40b5
Williano <paawilly17@gmail.com>
2020-04-05 02:49:51 -0500

    Update

---
+++ lib/widgets/custom_flexible_space_bar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+Widget customFlexibleSpaceBar(
+    {@required BuildContext context,
+    @required String title,
+    String assetImage,
+    String networkImageUrl}) {
+  return FlexibleSpaceBar(
+      title: Text(
+        "$title",
+        style: TextStyle(
+            color: Theme.of(context).primaryColorLight,
+            fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      background: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                          "$assetImage",
+                        ) ??
+                        NetworkImage("$networkImageUrl"),
+                    fit: BoxFit.fill)),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.7),
+          )
+        ],
+      ));
+}
