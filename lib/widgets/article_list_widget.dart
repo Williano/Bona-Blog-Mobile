9afbfb1623bd724cd6c816cd02e2c5f299a10e26
Williano <paawilly17@gmail.com>
2020-04-05 02:49:24 -0500

    Add reusable list builder

---
+++ lib/widgets/article_list_widget.dart
@@ -0,0 +1,23 @@
+import 'package:bona_blog/widgets/article_card_widget.dart';
+import 'package:flutter/material.dart';
+
+Widget articleListView({BuildContext context, List articles}) {
+  return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: articles.length,
+      itemBuilder: (BuildContext context, int articleIndex) {
+        return GestureDetector(
+            onTap: () {
+              print("Tab");
+            },
+            child: articleCard(
+                context: context,
+                articleAuthor: articles[articleIndex].author,
+                articleTitle: articles[articleIndex].title,
+                articleCategory: articles[articleIndex].category,
+                articleImageUrl: articles[articleIndex].imageURL,
+                articleDatePublishedOn: articles[articleIndex].datePublishedOn,
+                articleReadTime: articles[articleIndex].readTime,
+                articleNumberOfViews: articles[articleIndex].numberOfViews));
+      });
+}
