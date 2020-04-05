6ff2ff315c423755dd6ea3bef9d825c4d3757b00
Williano <paawilly17@gmail.com>
2020-04-05 17:41:41 -0500

    Update

--- lib/screens/undefined_screen.dart
+++ lib/screens/undefined_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class UndefinedScreen extends StatelessWidget {
   final String name;
 
-  UndefinedScreen({Key key, this.name}) : super(key: key);
+  const UndefinedScreen({Key key, this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
