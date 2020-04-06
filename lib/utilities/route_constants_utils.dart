4b196b936f19b95abaef9928f6f107da3836f12f
Williano <paawilly17@gmail.com>
2020-04-06 12:41:56 -0500

    Update filename

---
+++ lib/utilities/route_constants_utils.dart
@@ -0,0 +1,4 @@
+const String HomeScreenRoute = '/';
+const String FeedScreenRoute = "/feed";
+const String CategoriesScreenRoute = "/categories";
+const String CategoryArticlesListRoute = "/category/articles/list";
