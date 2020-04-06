92f0374593adf8034f7e27bc0d6feec4df546b84
Williano <paawilly17@gmail.com>
2020-04-06 12:34:52 -0500

    Update filename

---
+++ lib/utilities/custom_material_colors_utils.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> color = {
+  50: Color.fromRGBO(255, 92, 87, .1),
+  100: Color.fromRGBO(255, 92, 87, .2),
+  200: Color.fromRGBO(255, 92, 87, .3),
+  300: Color.fromRGBO(255, 92, 87, .4),
+  400: Color.fromRGBO(255, 92, 87, .5),
+  500: Color.fromRGBO(255, 92, 87, .6),
+  600: Color.fromRGBO(255, 92, 87, .7),
+  700: Color.fromRGBO(255, 92, 87, .8),
+  800: Color.fromRGBO(255, 92, 87, .9),
+  900: Color.fromRGBO(255, 92, 87, 1),
+};
+
+final MaterialColor customColor = MaterialColor(0xFF29908B, color);
