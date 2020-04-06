e55fa70d4ea565a577e91f21d58e7718f0f2a4f1
Williano <paawilly17@gmail.com>
2020-04-06 00:31:03 -0500

    Restructure into reusable component

---
+++ lib/widgets/static_flexible_space_bar_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+Widget staticFlexibleSpaceBar({
+  @required BuildContext context,
+  @required String title,
+  String assetImage,
+  String networkImageUrl,
+}) {
+  return FlexibleSpaceBar(
+    title: Text(
+      "$title",
+      maxLines: 3,
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+          color: Theme.of(context).primaryColorLight,
+          fontWeight: FontWeight.bold),
+    ),
+    centerTitle: true,
+    background: Stack(
+      children: <Widget>[
+        ClipRRect(
+          child: Image(
+            width: double.infinity,
+            height: double.infinity,
+            image: AssetImage(
+                  "$assetImage",
+                ) ??
+                NetworkImage("$networkImageUrl"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          color: Colors.black.withOpacity(0.7),
+        )
+      ],
+    ),
+  );
+}
