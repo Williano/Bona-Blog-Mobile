ad88eca8ff16512d65d7c28494c65b0dc9a4af75
Williano <paawilly17@gmail.com>
2020-04-06 00:30:43 -0500

    Restructure into reusable component

---
+++ lib/widgets/dynamic_sliver_app_bar_widget.dart
@@ -0,0 +1,21 @@
+import 'package:bona_blog/utilities/custom_material_color.dart';
+import 'package:bona_blog/widgets/dynamic_flexible_space_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+Widget dynamicSliverAppBar({
+  @required BuildContext context,
+  @required String title,
+  String assetImage,
+  String networkImageUrl,
+}) {
+  return SliverAppBar(
+      pinned: true,
+      expandedHeight: 200,
+      elevation: 0.0,
+      backgroundColor: customColor,
+      flexibleSpace: dyanmicFlexibleSpaceBar(
+          context: context,
+          title: title,
+          assetImage: assetImage,
+          networkImageUrl: networkImageUrl));
+}
