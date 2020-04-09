bd5e2a369967ece4b315bf625f9b2b8d5573315f
Williano <paawilly17@gmail.com>
2020-04-09 15:59:33 -0500

    Add global colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bona_blog/routes/router.dart';
 import 'package:bona_blog/screens/home_screen.dart';
-import 'package:bona_blog/utilities/custom_material_colors_utils.dart';
 import 'package:bona_blog/utilities/route_constants_utils.dart';
 // import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
@@ -29,10 +28,28 @@ class BonaBlog extends StatelessWidget {
       initialRoute: HomeScreenRoute,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: customColor,
         primaryColorLight: Colors.white,
         primaryColorDark: Colors.black,
-        backgroundColor: Color(0xFFEDF3F3),
+        backgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        bottomAppBarColor: Color(0xFF04323F),
+        appBarTheme: AppBarTheme(color: Colors.blueGrey[900]),
+        buttonColor: Color(0xFFD45812),
+        primaryColor: Color(0xFF04323F),
+        primaryTextTheme:
+            TextTheme(headline: TextStyle(color: Color(0xFFD45812))),
+        // primaryColor: Colors.blueGrey[900],
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+        //backgroundColor: Color(0xFFEDF3F3),
       ),
       home: HomeScreen(),
     );
