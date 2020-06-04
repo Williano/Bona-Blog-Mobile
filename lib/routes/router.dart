295d26cf4026e65864afb3d89410cbfa4e34b53c
Williano <paawilly17@gmail.com>
2020-06-04 17:47:12 -0500

    Refactor structure

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,5 +1,5 @@
-import 'package:bona_blog/ui/screens/category/category_articles_list_screen/category_articles_list_screen.dart';
-import 'package:bona_blog/ui/screens/category/category_list_screen/category_list_screen.dart';
+import 'package:bona_blog/ui/screens/category_screens/category_articles_list_screen/category_articles_list_screen.dart';
+import 'package:bona_blog/ui/screens/category_screens/category_list_screen/category_list_screen.dart';
 import 'package:bona_blog/ui/screens/feed_screen.dart';
 import 'package:bona_blog/ui/screens/home_screen.dart';
 import 'package:bona_blog/ui/screens/undefined_screen.dart';
