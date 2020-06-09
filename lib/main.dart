2c4351c6e492fe5e81bb2374bf4883f3e164c45e
Williano <paawilly17@gmail.com>
2020-06-09 02:44:09 -0500

    Update bloc

--- lib/main.dart
+++ lib/main.dart
@@ -32,44 +32,47 @@ Future<void> main() async {
 class BonaBlog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // locale: DevicePreview.of(context).locale, // <--- Add the locale
-      // builder: DevicePreview.appBuilder,
-      onGenerateRoute: generateRoute,
-      initialRoute: HomeScreenRoute,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColorLight: Colors.white,
-        primaryColorDark: Colors.black,
-        backgroundColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-        bottomAppBarColor: Color(0xFF04323F),
-        appBarTheme: AppBarTheme(color: Colors.blueGrey[900]),
-        buttonColor: Color(0xFFD45812),
-        primaryColor: Color(0xFF04323F),
-        primaryTextTheme:
-            TextTheme(headline1: TextStyle(color: Color(0xFFD45812))),
-        // primaryColor: Colors.blueGrey[900],
-        // Define the default font family.
-        fontFamily: 'Georgia',
-
-        // Define the default TextTheme. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline3: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
-        //backgroundColor: Color(0xFFEDF3F3),
-      ),
-      home: MultiBlocProvider(providers: [
+    return MultiBlocProvider(
+      providers: [
         BlocProvider<CategoryBloc>(
           create: (context) => servicelocator<CategoryBloc>(),
         ),
         BlocProvider<ArticleBloc>(
           create: (context) => servicelocator<ArticleBloc>(),
         ),
-      ], child: HomeScreen()),
+      ],
+      child: MaterialApp(
+        // locale: DevicePreview.of(context).locale, // <--- Add the locale
+        // builder: DevicePreview.appBuilder,
+        onGenerateRoute: generateRoute,
+        initialRoute: HomeScreenRoute,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColorLight: Colors.white,
+          primaryColorDark: Colors.black,
+          backgroundColor: Colors.white,
+          scaffoldBackgroundColor: Colors.white,
+          bottomAppBarColor: Color(0xFF04323F),
+          appBarTheme: AppBarTheme(color: Colors.blueGrey[900]),
+          buttonColor: Color(0xFFD45812),
+          primaryColor: Color(0xFF04323F),
+          primaryTextTheme:
+              TextTheme(headline1: TextStyle(color: Color(0xFFD45812))),
+          // primaryColor: Colors.blueGrey[900],
+          // Define the default font family.
+          fontFamily: 'Georgia',
+
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline3: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
+          //backgroundColor: Color(0xFFEDF3F3),
+        ),
+        home: HomeScreen(),
+      ),
     );
   }
 }
